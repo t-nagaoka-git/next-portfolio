00f3e8a2b19d09365fdcb66da77b83ddfa655d1f
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-04 08:52:08 +0900

    358 Link の使い方

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
+import Link from 'next/link';
 import * as style from '../styles/index.module.css';
 
 const index = () => {
-  return <h1 className={style.h1Text}>こんにちは</h1>;
+  return (
+    <>
+      <h1 className={style.h1Text}>こんにちは</h1>
+      <Link href="/contact">
+        <a>Contact ページへ移動</a>
+      </Link>
+    </>
+  );
 };
 
 export default index;
