7e5525165da870aafca404751af546f9344c5ac2
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-04 08:41:13 +0900

    264 CSS スタイルのあて方

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+import * as style from '../styles/index.module.css';
+
 const index = () => {
-  return <h1>こんにちは</h1>;
+  return <h1 className={style.h1Text}>こんにちは</h1>;
 };
 
 export default index;
